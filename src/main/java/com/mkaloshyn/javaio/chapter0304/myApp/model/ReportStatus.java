a9108548de4a5dc35f475f0f9e2987de7768adf9
MykhailoMike <mkaloshyn@gmail.com>
2020-03-29 19:37:21 +0300

    javaio console app. 1st part: Reports. Other parts to be added.

---
+++ src/main/java/com/mkaloshyn/javaio/chapter0304/myApp/model/ReportStatus.java
@@ -0,0 +1,6 @@
+package main.java.com.mkaloshyn.javaio.chapter0304.myApp.model;
+
+public enum ReportStatus {
+
+    APPROVED, UNDER_REVIEW, DELETED
+}
---
+++ src/main/java/com/mkaloshyn/javaio/chapter0304/myApp/repository/GenericRepository.java
@@ -0,0 +1,5 @@
+package main.java.com.mkaloshyn.javaio.chapter0304.myApp.repository;
+
+public interface GenericRepository<T, ID> {
+
+    }
